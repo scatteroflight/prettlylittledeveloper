7ac155ad251b8a70581c52fc6219e3b87868233f
scatteroflight <62908596+scatteroflight@users.noreply.github.com>
2020-10-01 15:05:52 +0100

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,1 +1,22 @@
 
+$.getJSON("https://twitter.com/statuses/user_timeline.json?screen_name=chrisspooner&count=1&callback=?",
+		function(data){
+			$.each(data, function(i,item){
+			ct = item.text;
+			ct = ct.replace(/http:\/\/\S+/g,  '<a href="$&">$&</a>');
+			ct = ct.replace(/\s(@)(\w+)/g,    ' @<a href="https://twitter.com/$2">$2</a>');
+	 		ct = ct.replace(/\s(#)(\w+)/g,    ' #<a href="http://search.twitter.com/search?q=%23$2">$2</a>');
+	   		$("#tweet").append('<p id="message">'+ct +'</p');
+	  	});
+	 });
+ 	 
+    jQuery(document).ready( function() {
+       var _config = {
+         username: 'spoongraphics',
+         count: 6,
+         period: '7day'
+     };
+    lastFmRecords.init(_config);
+    });
+
+	$('#flickrimages').flickrush({id:'10201900@N03' }
